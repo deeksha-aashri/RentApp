c2a6cb35068e05c88c3c5185834dd4f8ad982aa7
deeksha-aashri <deekshaaashri1@gmail.com>
2022-07-31 00:31:34 +0530

    :rocket: UI updated

--- App.js
+++ App.js
@@ -6,20 +6,24 @@ import Body from './Components/Body';
  import Container from '@mui/material/Container';
  import Grid from '@mui/material/Grid';
  import Filterlayer from './Components/Filterlayer';
+import context from './Components/Context';
 
 import {data} from './Components/data';
+import { useState } from 'react';
 
 function App() {
-  
+  const [type,setType]=useState("house");
   return (
     <>
     <Navbar/>
     <Container>
+      <context.Provider value={type}>
     <Middle/>
     <Filterlayer/>
     <Grid container spacing={5}>
      <Body/>
     </Grid>
+    </context.Provider >
     </Container>
     </>
     
--- Components/Filterlayer.js
+++ Components/Filterlayer.js
@@ -1,90 +1,112 @@
-import React ,{useState}from 'react'
+import React, { useState } from 'react'
 import { useContext } from 'react';
 import Paper from '@mui/material/Paper'
 import ButtonGroup from '@mui/material/ButtonGroup'
 import Button from '@mui/material/Button'
-import { MenuItem, Select, FormControl, FormHelperText, InputLabel,NativeSelect } from '@mui/material'
+import { MenuItem, Select, FormControl, FormHelperText, InputLabel, NativeSelect } from '@mui/material'
 import Box from '@mui/material/Box';
+import context from './Context';
+import { data } from './data';
 
-import {data} from './data';
 function Filterlayer() {
-    let result=data["hotels"];
-    
+    let result = data["hotels"];
+    let typearr = ["hotel", "house"]
+    const type = useContext(context);
     return (
 
         <Paper variant="outlined" elevation={3} sx={{ marginY: "2rem", height: "4rem" }} >
             <ButtonGroup variant="text" sx={{ display: "flex", justifyContent: "space-around", height: "3rem", padding: "0.5rem" }} >
 
-               
-            <FormControl >
+
+                <FormControl >
                     <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                  Location
+                        Location
                     </InputLabel>
                     <NativeSelect
-                        defaultValue={20}
+                        
                         inputProps={{
                             name: 'age',
                             id: 'uncontrolled-native',
                         }}
                     >
-                        <option value={10}>Ten</option>
-                        <option value={20}>Twenty</option>
-                        <option value={30}>Thirty</option>
+                        {result.map((obj)=>(
+                            <option>{obj.city}</option>
+                        )
+                            
+                        )}
                     </NativeSelect>
                 </FormControl>
 
 
                 <FormControl >
                     <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                    When
+                        When
                     </InputLabel>
                     <NativeSelect
-                        defaultValue={30}
+                        
                         inputProps={{
                             name: 'age',
                             id: 'uncontrolled-native',
                         }}
                     >
                         <option value={10}></option>
+
+                    </NativeSelect>
+                </FormControl>
+
+                <FormControl >
+                    <InputLabel variant="standard" htmlFor="uncontrolled-native">
+                        Price
+                    </InputLabel>
+                    <NativeSelect
                         
-                    </NativeSelect>
-                </FormControl>
-
-                <FormControl >
-                    <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                  Price
-                    </InputLabel>
-                    <NativeSelect
-                        defaultValue={30}
                         inputProps={{
                             name: 'age',
                             id: 'uncontrolled-native',
                         }}
                     >
-                        <option value={10}>Ten</option>
-                        <option value={20}>Twenty</option>
-                        <option value={30}>Thirty</option>
+                       {result.map((obj)=>{
+                        (obj["price"].slice(1,4))<500?
+                        (
+                            <option>Less than $500</option>
+                        )
+                        :
+                        (
+                            <option>More than 500</option>
+                        )
+                       })}
                     </NativeSelect>
                 </FormControl>
 
 
                 <FormControl >
                     <InputLabel variant="standard" htmlFor="uncontrolled-native">
-                 Property
+                        Property
                     </InputLabel>
                     <NativeSelect
-                        defaultValue={30}
                         inputProps={{
                             name: 'age',
                             id: 'uncontrolled-native',
                         }}
                     >
-                        <option value={10}>Ten</option>
-                        <option value={20}>Twenty</option>
-                        <option value={30}>Thirty</option>
+                        {
+                            result.map((obj)=>(
+                                obj.type=="house"?
+                                (
+                                    <option>House</option>
+                                )
+                                :
+                                (
+                                    <option>Hotel</option>
+                                )
+                                
+                            ))
+                        }
+
+
                     </NativeSelect>
                 </FormControl>
-                
+
                 <Button variant="contained" color="secondary">Search</Button>
             </ButtonGroup>
         </Paper>
